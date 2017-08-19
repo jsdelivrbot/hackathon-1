8a5a9701d24ccbea0bfb288bb4b14c0259298f9b
Andre Perez <andre_mperez@hotmail.com>
2017-08-19 17:51:48 -0300

    Create index.js

---
+++ web/static/javascript/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $('.pushpin-demo-nav').pushpin({
+    top: 0,
+    bottom: 1000,
+    offset: 0
+  });
+
+  $(".button-collapse").sideNav({
+
+    menuWidth: 250,
+    closeOnClick: true,
+    draggable: true
+  });
+});
