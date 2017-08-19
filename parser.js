8227cc2be8daa86b6a9379da8dfea1dff9b5768b
pedroteosousa <pedroteosousa@gmail.com>
2017-08-19 19:12:33 -0300

    updated parser

--- parser.js
+++ parser.js
@@ -4,4 +4,19 @@ var xml = fs.readFileSync('test.xml', 'utf8');
 var inspect = require('util').inspect;
  
 var obj = parse(xml);
-console.log(inspect(obj, { colors: true, depth: Infinity }));
+
+console.log(inspect(obj, {colors: true, depth: Infinity}));
+
+for (i in obj.root.children) {
+	var value = obj.root.children[i];
+	if (value.name == "steps") {
+		for (j in value.children) {
+			console.log(value.children[j].attributes.number + " - " + value.children[j].attributes.title)
+			var 
+			for (k in value.children[j].children) {
+				console.log(value.children[j].children[k]);
+
+			}
+		}
+	}
+}
