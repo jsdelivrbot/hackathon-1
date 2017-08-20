38fe5aae67b62246a0ad37f0865ec3b7b228d1c4
Andre Perez <andre_mperez@hotmail.com>
2017-08-20 10:51:40 -0300

    Add files via upload

---
+++ web/static/javascript/profile.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $('.collapsible').collapsible({
+
+    closeOnClick: false
+  });
+
+  $(".button-collapse").sideNav({
+
+    menuWidth: 300,
+    draggable: true,
+    closeOnClick: false
+  });
+});
